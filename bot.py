138f6d9cbc8ab5ba851f6832d5a9eadbf1a35c5b
chamathhesara <94973557+chamathhesara@users.noreply.github.com>
2022-02-27 00:18:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,9 +114,7 @@ class Chat:
             try:
                 self.send_audio(file_name)
                 self.delete_message(downloading_message)
-                self.send_message('😙Song downloaded!\nFeel the song🎵\nDeveloper : [Sanila Ranatunga✨](https://github.com/sanila2007)'),
-                                parse_mode="markdown"
-                    )
+                self.send_message('😙Song downloaded!\nFeel the song🎵\nDeveloper: Sanila Ranatunga✨😙')
                 print("\nDownloaded successfully!\n")
 
             except:
