dd19374bc88b350016b5c3abf7846518dcf6b344
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-04-23 14:02:40 +0530

    Update text_msg.py

--- text_msg.py
+++ text_msg.py
@@ -1,5 +1,7 @@
 import pyrogram
 
-START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n■ **If you cannot understand how this bot work you can learn it from** @sanilaassistant_bot \n\n■ **first start** @sanilaassistant_bot **> Learn Bots > Chose Song downloader bot**\n\n✨ Developer: Sanila Ranatunga\n✨ Feedback: [Submit by clicking](https://t.me/sanilaasssistant_bot)'
+START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\n' \
+                 'to download some songs. 🎶\n\n■ **If you cannot understand how this bot work you can learn it from** @sanilaassistant_bot \n\n■' \
+                 '**first start** @sanilaassistant_bot **> Learn Bots > Chose Song downloader bot**\n\n✨ Developer: Sanila Ranatunga\n✨ Feedback: [Submit by clicking](https://t.me/sanilaasssistant_bot)'
 
 
