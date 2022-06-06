101fb0ebe1acdc4dd9ec360949f85f3bcc7dcf50
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-06-06 15:28:57 +0530

    Updated v2.0.1

--- text_msg.py
+++ text_msg.py
@@ -4,8 +4,8 @@ START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just se
 
 CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n◉ [Give feedback](https://t.me/sanilaassistant_bot)\n◉ [Learn Bot](https://t.me/sanilaassistant_bot)"
 
-SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Oops! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n\n■ [Get help](https://t.me/sanilaassistant_bot)> Learn Bots > Chose Song downloader'
+SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Oops! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
 
-INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ If you cannot understand how this bot work you can learn it by [clicking this.](https://t.me/sanilaassistant_bot) \n\n■ First [start](https://t.me/sanilaassistant_bot)> Learn Bots > Chose Song downloader'
+INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
 
-TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n■ If you cannot understand how this bot work you can learn it by [clicking this.](https://t.me/sanilaassistant_bot) \n\n■ First [start](https://t.me/sanilaassistant_bot)> Learn Bots > Chose Song downloader'
+TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
