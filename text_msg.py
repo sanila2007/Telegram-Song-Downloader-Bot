6542cf1d1ad23d87a2e1e1484a7d234d9ebd0af9
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-06-25 21:55:41 +0530

    Update text_msg.py

--- text_msg.py
+++ text_msg.py
@@ -1,6 +1,6 @@
 import pyrogram
 
-START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n■ ProTips💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          (https://t.me/sanilaassistant_bot)[»Rate Bot](https://t.me/sanilaassistant_bot)\n                          (https://t.me/sanilaassistant_bot)[»Get help](https://t.me/sanilaassistant_bot)\n                          (https://t.me/sanilaassistant_bot)[»Give feedback](https://t.me/sanilaassistant_bot)'
+START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n■ ProTips💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
 CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n◉ [Give feedback](https://t.me/sanilaassistant_bot)\n◉ [Learn Bot](https://t.me/sanilaassistant_bot)"
 
@@ -10,4 +10,4 @@ INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song*
 
 TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
 
-#All rights reserved 
+
