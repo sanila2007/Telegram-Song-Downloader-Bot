9976303b17e3ebcfb46140a3da1d56c672fc6e31
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-06-29 19:55:45 +0530

    Update text_msg.py

--- text_msg.py
+++ text_msg.py
@@ -2,12 +2,12 @@ import pyrogram
 
 START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
-CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n◉ [Give feedback](https://t.me/sanilaassistant_bot)\n◉ [Learn Bot](https://t.me/sanilaassistant_bot)"
+CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n◉ "
 
-SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Oops! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
+SPOTIFY_INPUT_ERROR_TEXT_MSG = '‼️ *Oops! The bot does not support Spotify links!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n'
 
-INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
+INVALID_COMMAND_ERROR_TEXT_MSG = '‼️ *Oops! Invalid command!*\nTry: "*/song* _song name_"\nor: "*/song* _musician name - song name_"\n\n'
 
-TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n■ [Learn Bot](https://t.me/sanilaassistant_bot) \n■ [Get help](https://t.me/sanilaassistant_bot)'
+TOO_LONG_ERROR_TEXT_MSG = '‼️ *Oops! Video too long to convert!*\nOrder something 30 minutes or less.\n\n'
 
 
